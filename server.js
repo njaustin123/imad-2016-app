e8ed8392e1b4aa63dc9c5476a806ab7b9d455820
njaustin123 <njaustin123@gmail.com>
2016-09-27 17:43:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,62 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var article1 = {
+    title: 'Article by Austin',
+    heading: 'article',
+    date: 'Sep 27, 2016',
+    content:  `<p>
+                    This is the Content for for my first Article.
+                </p>`
+};
+
+function createHTML(data){
+var title = data.title;
+var header = data.header;
+var date = data.date;
+var content = data.content;
+
+var htmlTemplate =`
+<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+            <div>
+                <a href="/">Home</a>
+            </div>
+            <hr/>
+            <h3>
+                ${header}
+            </h3>
+            <div>
+                ${date}
+            </div>
+            <div>
+                <p>
+                    ${content}
+                </p>
+            </div>
+        </div>
+    </body>
+</html>
+`;
+
+return htmlTemplate;
+
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article1', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article1.html'));
+  //res.sendFile(path.join(__dirname, 'ui', 'article1.html'));
+  res.createHTML(article1);
 });
 
 app.get('/ui/style.css', function (req, res) {
