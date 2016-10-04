4418e2f14a47a223f5afc8b3e384a9a45a665b3f
njaustin123 <njaustin123@gmail.com>
2016-10-04 19:25:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,7 @@
 console.log('Loaded!');
 var element = document.getElementById("main-text");
 element.innerHTML = "New Value for Hello Austin";
+var logo = document.getElementById("madi");
+logo.onclick = function(){
+    logo.style.marginLeft = '100px';
+};
